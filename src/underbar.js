7010ff8500f80ed02ee614225427c83d2ee81fdc
Tegan Duong <teganduong@gmail.com>
2016-01-05 12:52:33 -0600

    Complete _.filter function to pass tests
    * Returns all elements of an array that pass a truth test

--- src/underbar.js
+++ src/underbar.js
@@ -80,6 +80,12 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var passed = [];
+    for (var i = 0; i < collection.length; i++) {
+      if (test(collection[i]))
+        passed.push(collection[i]);
+    }
+    return passed;
   };
 
   // Return all elements of an array that don't pass a truth test.
