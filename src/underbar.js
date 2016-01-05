81b67065658c160c50e59120e02c65de4417fc27
Tegan Duong <teganduong@gmail.com>
2016-01-05 13:23:41 -0600

    Complete _.reject function to pass tests

--- src/underbar.js
+++ src/underbar.js
@@ -92,6 +92,12 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var rejected = [];
+    for (var i = 0; i < collection.length; i++) {
+      if (!test(collection[i]))
+        rejected.push(collection[i]);
+    }
+    return rejected;
   };
 
   // Produce a duplicate-free version of the array.
