c1db4ac5caadca8064a9358137dc83000cd43830
Tegan Duong <teganduong@gmail.com>
2016-02-09 16:18:13 -0600

    Update _.filter to reuse _.each function

--- src/underbar.js
+++ src/underbar.js
@@ -81,10 +81,11 @@
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
     var passed = [];
-    for (var i = 0; i < collection.length; i++) {
-      if (test(collection[i]))
-        passed.push(collection[i]);
-    }
+    _.each(collection, function(element) {
+      if (test(element)) {
+        passed.push(element);
+      }
+    });
     return passed;
   };
 
